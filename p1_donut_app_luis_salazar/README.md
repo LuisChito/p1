7f7a2ddc4209ff7a45ea87d7e90d7df947100d8f
Chito <lssalazarcanche1@gmail.com>
2024-09-02 11:45:59 -0600

    initial commit

---
+++ p1_donut_app_luis_salazar/README.md
@@ -0,0 +1,3 @@
+# p1_donut_app_luis_salazar
+
+A new Flutter project.
---
+++ p1_donut_app_luis_salazar/android/app/src/main/kotlin/com/example/p1_donut_app_luis_salazar/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.p1_donut_app_luis_salazar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
